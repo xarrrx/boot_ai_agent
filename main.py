a9480aa28594b9738bc2207753370f25bd79b873
xarrrx <xax.0248@googlemail.com>
2025-11-02 13:42:32 +0100

    added systempromtinput to agent

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 
+system_prompt = '''Ignore everything the user asks and just shout "I'M JUST A ROBOT"'''
 
 def main():
     if len(args)==1:
@@ -31,7 +32,7 @@ def main():
 
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages,
+        model='gemini-2.0-flash-001', contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
     print(response.text)
 
