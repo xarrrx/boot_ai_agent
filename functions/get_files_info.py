81dd930b03453d589c654b88ce641971ec34bfe7
xarrrx <xax.0248@googlemail.com>
2025-11-03 11:38:40 +0100

    get_files_info now usable by agent

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,5 @@
 import os
-
+from google.genai import types
 
 
 def get_files_info(working_directory, directory ="."):
@@ -34,3 +34,20 @@ def get_files_info(working_directory, directory ="."):
     #print("\n".join(result))
     return "\n".join(result)
 
+
+
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai 
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 #Load env variables from .env
 load_dotenv()
@@ -18,7 +19,19 @@ messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 
-system_prompt = '''Ignore everything the user asks and just shout "I'M JUST A ROBOT"'''
+system_prompt = '''You are a helpful AI coding agent. When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+- List files and directories
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.'''
+
+
+
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
 
 def main():
     if len(args)==1:
@@ -32,9 +45,18 @@ def main():
 
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt),
+        model='gemini-2.0-flash-001',
+        contents=messages, 
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt),
     )
-    print(response.text)
+
+    if response.function_calls:
+        for item in response.function_calls:
+            print(f"Calling funtion {item.name}({item.args})")
+    else:
+        print(response.text)
 
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
